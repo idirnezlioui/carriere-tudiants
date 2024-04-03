148292166cb5e8e8a10d8780790488dd2eef6fde
idir <idirnezlioui4@gmail.com>
2024-04-03 16:09:55 +0200

    Update App.js

--- carriere_edt/src/App.js
+++ carriere_edt/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import PageAcceuil from './componenet/PageAcceuil';
 
 function App() {
   return (
     <div className="App">
-      
+      <PageAcceuil/>
     </div>
   );
 }
