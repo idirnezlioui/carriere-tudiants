8fadf80676e03e9e41a2e503810e393450b23cf4
idir <idirnezlioui4@gmail.com>
2024-04-04 04:09:51 +0200

    Update PageAcceuil.js

--- carriere_edt/src/componenet/PageAcceuil.js
+++ carriere_edt/src/componenet/PageAcceuil.js
@@ -2,7 +2,13 @@ import { useState, useEffect } from "react";
 import NavBarre from "./NavBarre";
 function pageAcceuil() {
     return(
-        <NavBarre/>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-12">
+                    <NavBarre/>
+                </div>
+            </div>
+        </div>
     )
 }
 export default pageAcceuil;
