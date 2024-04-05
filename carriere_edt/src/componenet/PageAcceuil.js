6e8bccaf1b1c072b98c4b812c5b39690aeee4058
idir <idirnezlioui4@gmail.com>
2024-04-05 03:48:05 +0200

    Update PageAcceuil.js

--- carriere_edt/src/componenet/PageAcceuil.js
+++ carriere_edt/src/componenet/PageAcceuil.js
@@ -1,14 +1,28 @@
-import { useState, useEffect } from "react";
+//import { useState, useEffect } from "react";
 import NavBarre from "./NavBarre";
+import Body from "./Body";
+import Footer from "./footer";
+
 function pageAcceuil() {
-    return(
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-md-12">
-                    <NavBarre/>
-                </div>
-            </div>
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <NavBarre />
+          </div>
         </div>
-    )
+        <div className="row">
+          <div className="col-md-12 col-sm-12 col-xm-12">
+            <h1 id="titre">
+              Carrière étudiant t'accompagne tout au long de ta vie d'étudiant
+            </h1>
+          </div>
+        </div>
+      </div>
+      <Body />
+      <Footer />
+    </>
+  );
 }
 export default pageAcceuil;
