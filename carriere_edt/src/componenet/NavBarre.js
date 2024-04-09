4f506d7fea93f460ff7cb6ad04188a3a0712e2c4
idir <idirnezlioui4@gmail.com>
2024-04-09 08:48:42 +0200

    Update NavBarre.js

--- carriere_edt/src/componenet/NavBarre.js
+++ carriere_edt/src/componenet/NavBarre.js
@@ -1,40 +1,71 @@
-import { useState, useEffect } from "react";
-import "../style/navbar.css"
-import Logo from"../images/LOGO_Rond.png"
+//import { useState, useEffect } from "react";
+import "../style/navbar.css";
+import Logo from "../images/LOGO_Rond.png";
+import { Sticky } from "react-sticky";
 function NavBarre() {
-    return(
-        <nav className="navbar navbar-expand-lg bg-body-tertiary" id="navbar">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#" >
-                    <img src={Logo} className="img-thumbnail" alt="Logo" id="image"/>
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top" id="navbar">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src={Logo} className="img-thumbnail" alt="Logo" id="image" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active acc" aria-current="page" href="#">
+                  Acceuil
                 </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active acc" aria-current="page" href="#">Acceuil</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Ta voie</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Ton stage</a>
-                        </li>
-                          
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Ton altérnance</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Ton job</a>
-                        </li>
-                    </ul>
-                    <button className="btn btn-primary"  id="bt1">Se connecter</button>
-                    <button type="button" className="btn btn-secondary" id="bt2">S'insecrire</button>
-                </div>
-            </div>
-        </nav>
-    )
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Ta voie
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Ton stage
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Ton altérnance
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Ton job
+                </a>
+              </li>
+            </ul>
+            <button className="btn btn-primary" id="bt1">
+              Se connecter
+            </button>
+            <button type="button" className="btn btn-secondary" id="bt2">
+              S'insecrire
+            </button>
+          </div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-12 col-sm-12 col-xm-12">
+          <h1 id="titre">
+            Carrière étudiant t'accompagne tout au long de ta vie d'étudiant
+          </h1>
+        </div>
+      </div>
+    </>
+  );
 }
 export default NavBarre;
