e633c735a4a644ee785ce2876c9521d8ff7fcc4b
idir <idirnezlioui4@gmail.com>
2024-04-13 00:01:35 +0200

    Update Body.js

--- carriere_edt/src/componenet/Body.js
+++ carriere_edt/src/componenet/Body.js
@@ -1,30 +1,44 @@
 
 import "../style/body.css"
+
 function Body() {
     return(
         <div className="container-fluid">
-                <div className="row"id="bd" >
-                    <div className="col-md-4 col-sm-12" id="im1">
-                        <h2 id="tit2">Ta voie</h2>
-                        <p id="par">Ton cursus</p>
-                        <p id="par">Ton école</p>
-                        <button type="button" className="btn btn-warning" id="bt">Rechercher</button>
+            <div className="row" id="bd">
+                <div className="col-lg-4 col-md-6 col-sm-12 mb-4">
+                    <div className="card" id="im1">
+                        <div className="card-body">
+                            <h2 className="card-title" id="tit2">Ta voie</h2>
+                            <p className="card-text" id="par">Ton cursus</p>
+                            <p className="card-text" id="par">Ton école</p>
+                            <button type="button" className="btn btn-warning" id="bt">Rechercher</button>
+                        </div>
                     </div>
-                    <div className="col-md-4"  id="im2">
-                    <h2 id="tit2">Ta entreprise</h2>
-                        <p id="par">stage</p>
-                        <p id="par">Altérnanace</p>
-                        <button type="button" className="btn btn-warning" id="bt">Rechercher</button>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-12 mb-4">
+                    <div className="card" id="im2">
+                        <div className="card-body">
+                            <h2 className="card-title" id="tit2">Ta entreprise</h2>
+                            <p className="card-text" id="par">Stage</p>
+                            <p className="card-text" id="par">Altérnanace</p>
+                            <button type="button" className="btn btn-warning" id="bt">Rechercher</button>
+                        </div>
                     </div>
-                    <div className="col-md-4"  id="im3">
-                    <h2 id="tit2">Ton emploi</h2>
-                        <p id="par">Job d'étudiant</p>
-                        <p id="par">Premier Emploi</p>
-                        <p id="par">Job d'été</p>
-                        <button type="button" className="btn btn-warning" id="bt">Rechercher</button>
+                </div>
+                <div className="col-lg-4 col-md-12 col-sm-12 mb-4">
+                    <div className="card" id="im3">
+                        <div className="card-body">
+                            <h2 className="card-title" id="tit2">Ton emploi</h2>
+                            <p className="card-text" id="par">Job d'étudiant</p>
+                            <p className="card-text" id="par">Premier Emploi</p>
+                            <p className="card-text" id="par">Job d'été</p>
+                            <button type="button" className="btn btn-warning" id="bt">Rechercher</button>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
     )
 }
+
 export default Body;
