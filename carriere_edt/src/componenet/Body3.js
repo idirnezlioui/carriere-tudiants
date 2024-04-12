d96ec8eb74d0630eac9148d3204e3c79c85ba816
idir <idirnezlioui4@gmail.com>
2024-04-13 00:01:42 +0200

    Update Body3.js

--- carriere_edt/src/componenet/Body3.js
+++ carriere_edt/src/componenet/Body3.js
@@ -1,86 +1,210 @@
 import "../style/body3.css";
+
 function Bodytree() {
   return (
-    <div className="complet">
-      <div className="container">
-        <div className="row">
-          <h3 className="text-start">Secteures en forte demande</h3>
+    <div className="container">
+      <div className="row">
+        <h3 className="text-start">Secteurs en forte demande</h3>
+      </div>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Aéronautique
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Tech
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Distribution
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Aliments et boissons
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Industrie
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Santé
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="row">
-          <nav class="navbar navbar-expand-lg ">
-            <div class="container-fluid">
-              <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      Aéronautique
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      Tech
-                    </a>
-                  </li>
+      </nav>
 
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      Distribution
+      <div id="carouselExample" className="carousel slide">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div className="row">
+              <div className="col-lg-3 col-md-4 col-sm-12">
+                <div className="card">
+                  <img
+                    className="card-img-top"
+                    src=""
+                    alt="Card image"
+                  />
+                  <div className="card-body">
+                    <h4 className="card-title">John Doe</h4>
+                    <p className="card-text">Some example text.</p>
+                    <a href="#" className="btn btn-primary">
+                      Suivre
                     </a>
-                  </li>
-
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      Aliments et boissons
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-4 col-sm-12">
+                <div className="card">
+                <img
+                    className="card-img-top"
+                    src=""
+                    alt="Card image"
+                  />
+                  <div className="card-body">
+                    <h4 className="card-title">John Doe</h4>
+                    <p className="card-text">Some example text.</p>
+                    <a href="#" className="btn btn-primary">
+                      Suivre
                     </a>
-                  </li>
-
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      Industrie
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-4 col-sm-12">
+                <div className="card">
+                <img
+                    className="card-img-top"
+                    src=""
+                    alt="Card image"
+                  />
+                  <div className="card-body">
+                    <h4 className="card-title">John Doe</h4>
+                    <p className="card-text">Some example text.</p>
+                    <a href="#" className="btn btn-primary">
+                      Suivre
                     </a>
-                  </li>
-
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      Santé
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-4 col-sm-12">
+                <div className="card">
+                <img
+                    className="card-img-top"
+                    src=""
+                    alt="Card image"
+                  />
+                  <div className="card-body">
+                    <h4 className="card-title">John Doe</h4>
+                    <p className="card-text">Some example text.</p>
+                    <a href="#" className="btn btn-primary">
+                      Suivre
                     </a>
-                  </li>
-                </ul>
+                  </div>
+                </div>
               </div>
             </div>
-          </nav>
-        </div>
-      </div>
-
-      <div id="carouselExample" class="carousel slide">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
           </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="..." />
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="..." />
+          <div className="carousel-item">
+            <div className="row">
+              <div className="col-lg-3 col-md-4 col-sm-12">
+                <div className="card">
+                <img
+                    className="card-img-top"
+                    src=""
+                    alt="Card image"
+                  />
+                  <div className="card-body">
+                    <h4 className="card-title">John Doe</h4>
+                    <p className="card-text">Some example text.</p>
+                    <a href="#" className="btn btn-primary">
+                      Suivre
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-4 col-sm-12">
+                <div className="card">
+                <img
+                    className="card-img-top"
+                    src=""
+                    alt="Card image"
+                  />
+                  <div className="card-body">
+                    <h4 className="card-title">John Doe</h4>
+                    <p className="card-text">Some example text.</p>
+                    <a href="#" className="btn btn-primary">
+                      Suivre
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-4 col-sm-12">
+                <div className="card">
+                <img
+                    className="card-img-top"
+                    src=""
+                    alt="Card image"
+                  />
+                  <div className="card-body">
+                    <h4 className="card-title">John Doe</h4>
+                    <p className="card-text">Some example text.</p>
+                    <a href="#" className="btn btn-primary">
+                      Suivre
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-4 col-sm-12">
+                <div className="card">
+                   <img
+                    className="card-img-top"
+                    src=""
+                    alt="Card image"
+                  />
+                  <div className="card-body">
+                    <h4 className="card-title">John Doe</h4>
+                    <p className="card-text">Some example text.</p>
+                    <a href="#" className="btn btn-primary">
+                      Suivre
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <button
-          class="carousel-control-prev"
+          className="carousel-control-prev"
           type="button"
           data-bs-target="#carouselExample"
-          data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
         </button>
         <button
-          class="carousel-control-next"
+          className="carousel-control-next"
           type="button"
           data-bs-target="#carouselExample"
-          data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
         </button>
       </div>
     </div>
   );
 }
+
 export default Bodytree;
