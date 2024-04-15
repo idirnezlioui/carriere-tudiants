2ca0277f78c98665fd2204298055b4bb84e0300c
idir <idirnezlioui4@gmail.com>
2024-04-15 10:16:57 +0200

    Update PageAcceuil.js

--- carriere_edt/src/componenet/PageAcceuil.js
+++ carriere_edt/src/componenet/PageAcceuil.js
@@ -1,6 +1,5 @@
 //import { useState, useEffect } from "react";
 import NavBarre from "./NavBarre";
-import Body from "./Body";
 import Footer from "./footer";
 import SecondBody from "./Body2";
 import Bodytree from "./Body3";
@@ -15,10 +14,10 @@ function pageAcceuil() {
           </div>
         </div>
       </div>
-      <Body />
+
       <SecondBody />
-      <Bodytree/>
-      <BodyForth/>
+      <Bodytree />
+      <BodyForth />
       <Footer />
     </>
   );
